f11deab77809a5db1f9adae87097c33ad82ee8b7
alnwangchi <alnwangchi@gmail.com>
2023-02-16 22:49:01 +0800

    feat: create simple components

---
+++ src/constans/navData.js
@@ -0,0 +1,20 @@
+const navData = [
+  {
+    name: '關於我',
+    path: '/about/',
+    icon: {
+      name: 'ri:account-circle-fill',
+      side: 'left',
+    },
+  },
+  {
+    name: '文章',
+    path: '/blog/',
+    icon: {
+      name: 'ri:book-3-line',
+      side: 'left',
+    },
+  },
+];
+
+export default navData;
