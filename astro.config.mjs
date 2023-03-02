79207c3294f11b668cfd07f51408544b9e7c389a
alnwangchi <alnwangchi@gmail.com>
2023-03-02 23:59:49 +0800

    env: react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), react()]
 });
