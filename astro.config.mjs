9b84fd6cbac8d672406ef2e0d94ba133aee3e325
alnwangchi <alnwangchi@gmail.com>
2023-03-03 00:09:07 +0800

    env: tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import image from '@astrojs/image';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   // ...
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), react()]
+  }), react(), tailwind()]
 });
