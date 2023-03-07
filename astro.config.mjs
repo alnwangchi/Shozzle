ab5b8b12f3c8a79592b7098cf3d3008a0ebaf5da
alnwangchi <alnwangchi@gmail.com>
2023-03-08 00:25:22 +0800

    fix: don't know output: 'server' mean so comment tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), react(), tailwind()],
-  output: "server",
-  adapter: netlify()
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    react(),
+    tailwind(),
+  ],
+  // output: 'server',
+  adapter: netlify(),
 });
