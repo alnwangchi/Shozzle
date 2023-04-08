0c0fde03b42763f75e3b37199d42f685d926cec4
alnwangchi <alnwangchi@gmail.com>
2023-04-08 18:30:54 +0800

    fix: lock scroll when menu is open

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,15 @@
+const body = document.querySelector('body');
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', function () {
   this.classList.toggle('is-active');
   menu.classList.toggle('is-active');
+
+  // menu 展開時禁止滾動
+  if (body.style.overflow === 'hidden') {
+    body.style.overflow = '';
+  } else {
+    body.style.overflow = 'hidden';
+  }
 });
