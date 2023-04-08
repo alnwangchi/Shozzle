e0dcb312bc61140b0ce463493325b9e5109a2613
alnwangchi <alnwangchi@gmail.com>
2023-04-08 19:39:03 +0800

    env: add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,20 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    react(),
-    tailwind(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), react(), tailwind(), mdx()],
   // output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
