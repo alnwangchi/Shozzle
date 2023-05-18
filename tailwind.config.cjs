aaf81c43d4291149489204c6370978ef5bf95081
alnwangchi <alnwangchi@gmail.com>
2023-05-18 20:20:00 +0800

    env: style settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#101010',
+        secondary: '#999',
+        emphasize: '#efe960',
+        text: '#292929',
+        soft: '#f0f0f0'
+      }
+    },
     fontFamily: {
       orbitron: ['Orbitron']
     }
