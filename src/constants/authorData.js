e581c8e557adec72441dfb02fe81b27f1a87f960
tsungyu <tsungyu927@gmail.com>
2023-06-04 15:21:53 +0800

    chore: update layout of author detail page

--- src/constants/authorData.js
+++ src/constants/authorData.js
@@ -3,19 +3,27 @@ const authorData = [
     name: 'Allen Wang',
     avatar: '/images/Allen_Avatar.png',
     position: 'Frontend',
-    skills: []
+    skills: [],
+    links: {}
   },
   {
     name: 'Josh Chou',
     avatar: '/images/Josh_Avatar.png',
     position: 'Frontend',
-    skills: []
+    skills: [],
+    links: {}
   },
   {
     name: 'Jamie Chien',
     avatar: '/images/Jamie_Avatar.png',
     position: 'Frontend',
-    skills: ['React', 'Javascript (Typescript)', 'NodeJS']
+    skills: ['React', 'Javascript (Typescript)', 'NodeJS'],
+    links: {
+      website: 'https://tsungyu927.github.io/',
+      email: 'mailto: tsungyu927@gmail.com',
+      linkedin: 'https://linkedin.com/in/tsungyu927',
+      github: 'https://github.com/tsungyu927'
+    }
   }
 ];
 
