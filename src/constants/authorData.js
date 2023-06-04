f060d16b434d5b87e9d2813113ba47531f04ddfc
tsungyu <tsungyu927@gmail.com>
2023-06-04 13:24:32 +0800

    feat: add author list page

--- src/constants/authorData.js
+++ src/constants/authorData.js
@@ -1,15 +1,21 @@
 const authorData = [
   {
     name: 'Allen Wang',
-    avatar: '/images/Allen_Avatar.png'
+    avatar: '/images/Allen_Avatar.png',
+    position: 'Frontend',
+    skills: []
   },
   {
     name: 'Josh Chou',
-    avatar: '/images/Josh_Avatar.png'
+    avatar: '/images/Josh_Avatar.png',
+    position: 'Frontend',
+    skills: []
   },
   {
     name: 'Jamie Chien',
-    avatar: '/images/Jamie_Avatar.png'
+    avatar: '/images/Jamie_Avatar.png',
+    position: 'Frontend',
+    skills: ['React', 'Javascript (Typescript)', 'NodeJS']
   }
 ];
 
