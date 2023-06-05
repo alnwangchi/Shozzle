b87edd67ec7b7131139e6597df90596facdb91bb
tsungyu <tsungyu927@gmail.com>
2023-06-05 20:24:24 +0800

    chore: update author info

--- src/constants/authorData.js
+++ src/constants/authorData.js
@@ -2,24 +2,47 @@ const authorData = [
   {
     name: 'Allen Wang',
     avatar: '/images/Allen_Avatar.png',
+    avatar_square: '/images/Allen_Avatar_Square.jpeg',
     position: 'Frontend',
-    skills: [],
-    links: {}
+    intro: 'good good',
+    skills: [
+      'React',
+      'Javascript (Typescript)',
+      'TailwindCSS',
+      'SCSS',
+      'Nextjs',
+      'Astro',
+      'MUI',
+      'Ant Design'
+    ],
+    links: {
+      website: '',
+      email: 'mailto: alnwangchi@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/allen-wang-4407251b9/',
+      github: 'https://github.com/alnwangchi'
+    }
   },
   {
     name: 'Josh Chou',
     avatar: '/images/Josh_Avatar.png',
+    avatar_square: '/images/Josh_Avatar_Square.jpeg',
     position: 'Frontend',
-    skills: ['React', 'Javascript (Typescript)', 'NodeJS'],
-    links: {}
+    intro: '>dev/null',
+    skills: ['React', 'Javascript (Typescript)', 'Docker', 'K8s'],
+    links: {
+      website: '',
+      email: 'mailto: chopperinthecar0319@gmail.com',
+      github: 'https://github.com/joshtinhan'
+    }
   },
   {
     name: 'Jamie Chien',
-    avatar: '/images/Jamie_Avatar.png',
+    avatar: '/images/Jamie_Avatar.jpeg',
+    avatar_square: '/images/Jamie_Avatar_Square.jpeg',
     position: 'Frontend',
     intro:
       'Passionate about creating useful and user-friendly websites to solve problems encountered in daily life.',
-    skills: ['React', 'Javascript (Typescript)', 'NodeJS'],
+    skills: ['React', 'Javascript (Typescript)', 'NodeJS', 'TailwindCSS', 'Ant Design'],
     links: {
       website: 'https://tsungyu927.github.io/',
       email: 'mailto: tsungyu927@gmail.com',
