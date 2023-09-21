e88148fe062e5cd8a62cbba813e9f3076f560e62
tsungyu927 <tsungyu927@gmail.com>
2023-09-21 22:36:24 +0800

    fix: resolve the bug of the ShareButton module

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-  FacebookShareButton,
-  LineShareButton,
-  LinkedinShareButton,
-  FacebookIcon,
-  LineIcon,
-  LinkedinIcon
-} from 'react-share';
+import FacebookShareButton from 'react-share/lib/FacebookShareButton';
+import FacebookIcon from 'react-share/lib/FacebookIcon';
+import LineShareButton from 'react-share/lib/LineShareButton';
+import LineIcon from 'react-share/lib/LineIcon';
+import LinkedinShareButton from 'react-share/lib/LinkedinShareButton';
+import LinkedinIcon from 'react-share/lib/LinkedinIcon';
 
 interface ShareButtonsProps {
   pathname: string;
@@ -22,15 +20,37 @@ const ShareButtons = (props: ShareButtonsProps) => {
   const url = `${DOMAIN_URL}${pathname}`;
 
   return (
-    <div className="flex space-x-2">
-      <FacebookShareButton url={url} quote={title}>
-        <FacebookIcon size={30} round iconFillColor="#101010" />
+    <div className="flex space-x-1">
+      <FacebookShareButton
+        url={url}
+        quote={title}
+        className="circle-btn animation-1 flex self-end shrink-0"
+      >
+        <span className="icon">
+          <FacebookIcon size={30} round iconFillColor="#101010" />
+        </span>
+        <div className="circle-bg" />
       </FacebookShareButton>
-      <LineShareButton url={url} title={title}>
-        <LineIcon size={30} round iconFillColor="#101010" />
+      <LineShareButton
+        url={url}
+        title={title}
+        className="circle-btn animation-1 flex self-end shrink-0"
+      >
+        <span className="icon">
+          <LineIcon size={30} round iconFillColor="#101010" />
+        </span>
+        <div className="circle-bg" />
       </LineShareButton>
-      <LinkedinShareButton url={url} title={title} summary={summary}>
-        <LinkedinIcon size={30} round iconFillColor="#101010" />
+      <LinkedinShareButton
+        url={url}
+        title={title}
+        summary={summary}
+        className="circle-btn animation-1 flex self-end shrink-0"
+      >
+        <span className="icon">
+          <LinkedinIcon size={30} round iconFillColor="#101010" />
+        </span>
+        <div className="circle-bg" />
       </LinkedinShareButton>
     </div>
   );
